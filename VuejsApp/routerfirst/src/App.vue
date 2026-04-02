bf1729194c88f6c35bff3932e09264ab88608d80
hurrayPersimmon <rahel337@naver.com>
2026-04-02 16:11:09 +0900

    Vue js Router

---
+++ VuejsApp/routerfirst/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container py-4">
+    <Header />
+    <div class="mt-4">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Header from './components/Header.vue';
+</script>
+
+<style scoped></style>
