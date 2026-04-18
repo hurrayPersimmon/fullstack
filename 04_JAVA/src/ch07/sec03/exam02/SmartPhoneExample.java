319973f8f6acc2f55f42bf0f55cd67235d34fc60
hurrayPersimmon <rahel337@naver.com>
2026-04-18 17:34:00 +0900

    java 과제 수정

--- 04_JAVA/src/ch07/sec03/exam02/SmartPhoneExample.java
+++ 04_JAVA/src/ch07/sec03/exam02/SmartPhoneExample.java
@@ -4,7 +4,8 @@ public class SmartPhoneExample {
 
     public static void main(String[] args) {
         SmartPhone smartPhone = new SmartPhone("iphone","purple");
-        System.out.println(smartPhone);
+        System.out.println(smartPhone.model);
+        System.out.println(smartPhone.color);
     }
 
 }
--- 04_JAVA/src/ch07/sec10/exam01/Phone.java
+++ 04_JAVA/src/ch07/sec10/exam01/Phone.java
@@ -1,6 +1,6 @@
 package ch07.sec10.exam01;
 
-public class Phone {
+public abstract class Phone {
     String owner;
 
     Phone(String owner) {
